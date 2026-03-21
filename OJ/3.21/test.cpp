d3c59efe9caf64e22b70e7f5dfcac46d6511ce29
wwwdfwew <15237652609@163.com>
2026-03-21 20:10:22 +0800

    oj

---
+++ OJ/3.21/test.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+    string* s = new string[10]{ "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
+    // string s[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+public:
+    void Combine(string digits, size_t level, string combineStr, vector<string>& v)
+    {
+        if (level == digits.size())
+        {
+            v.push_back(combineStr);
+            return;
+        }
+        size_t num = digits[level] - '0';
+        string str = s[num];
+        for (size_t i = 0; i < str.size(); i++)
+        {
+            Combine(digits, level + 1, combineStr + str[i], v);
+        }
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> v;
+        if (digits.empty())
+            return v;
+        Combine(digits, 0, "", v);
+        return v;
+    }
+};
