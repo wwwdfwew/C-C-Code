2f037b0614bf879ce716d051038ed3cd7d8fe9e9
wwwdfwew <15237652609@163.com>
2026-07-17 18:54:41 +0800

    代码

--- 复习练习代码/string/test/string.cpp
+++ 复习练习代码/string/test/string.cpp
@@ -147,6 +147,81 @@ int main()
 			}
 		}
 		//在Linux环境下，相同的代码，扩容的规律不同。
-		//reserve()
+//----------------------------------reserve():也是扩容，用于提前开空间，适合那些知道需要多少空间的情况
+		string s8("hello world");
+		s8.reserve(100);
+		cout << s8.size() << endl;
+		cout << s8.capacity() << endl;
+		//缩容:当我们的字符串数组中没有数据，且开的空间小于capacity时，就会缩容,但这是不稳定的，在不同环境下结果可能不一样
+		s8.clear();
+		s8.reserve(20);
+		cout << s8.size() << endl;
+		cout << s8.capacity() << endl;
+		cout << endl;
+//------------------------resize():也是扩容，但是可以初始化，因为初始化填有数据，因此size也会改变,若不给定初始化的值，则默认为0
+		string s9("hello world");
+		s9.resize(100);
+		cout << s9.size() << endl;
+		cout << s9.capacity() << endl;
+		//当n<length时，就会删除数据，但是不缩容
+		s9.resize(20);
+		cout << s9.size() << endl;
+		cout << s9.capacity() << endl;
+		cout << endl;
+////--------------------------at：功能和[]一样，不同的是对错误的处理，[]是断言，at是抛出异常
+		string s10 = "hello world";
+		//s10[20];//直接中止程序
+		//s10.at(20);//抛出异常
+
+		//捕获异常：
+		/*try {
+			s10.at(30);
+		}
+		catch(const exception& e)
+		{
+			cout << e.what() << endl;
+		}*/
+
+////--------------------------insert：在任意位置插入字符或者插入n个字符
+		string s11 = "hello world";
+		s11.insert(5, "code");
+		s11.insert(0, 1,'x');//在第一个位置插入一个字符
+		cout << s11 << endl;
+///---------------------------erase:从pos位置开始删除n个字符
+		s11.erase(0,1);//头删
+		s11.erase(5, 4);//从第五个位置删除四个字符
+		cout << s11 << endl;
+//////insert和erase都需要提供位置和多个个字符，除了用迭代器时。
+
+
+//////------------------------replace:从pos位置开始替换n个字符，其实是将一个区间内的数据替换成指定的字符串
+
+		s11.replace(0, 5, "world");
+		cout << s11<<endl;
+
+/////--------------------------c_str:返回string 对象中的char *类型变量
+	
+		cout << s11.c_str() << endl;//因为C++是兼容C的，因此有些场景C可以做，C++不能做，就需要用到这些方法
+		cout << endl;
+////---------------------------find:查找字符，找到则返回对应元素下标,未找到则返回-1
+		int num = s11.find("l");
+		cout << num << endl;
+////---------------------------rfind:从后往前找，找到则返回对应元素下标,未找到则返回-1
+		int num1 = s11.rfind('l');
+		cout << num1 << endl;
+///---------------------------operator +:字符串相加，但是不改变对象本身
+///---------------------------operator+=:改变自身
+///---------------------------getline:从标准输入里面获取字符串存储到指定对象，可指定分割符，默认为换行符
+		/*string get;
+		getline(cin, get);
+		cout << get << endl;*/
+////作用是什么呢，因为用cin>>get接收字符串，空格和分隔符也会作为分隔符，因此要是输入带有空格的字符串，在一些场景的使用中
+////会出现问题。
+
+////---------------------------substr:从pos位置开始取n个字符
+		string s12 = "hello world";
+		string s13 = s12.substr(0, 5);
+		cout << endl << s13 << endl;
+
 	return 0;
 }
